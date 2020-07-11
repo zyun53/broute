fe76f85e95256f7a02b65211deb680f49b4d10d5
Isida Zyun'iti <zyun@moin.jp>
2020-07-11 05:55:33 +0000

    Add express api

--- index.js
+++ index.js
@@ -1,34 +1,83 @@
 require('dotenv').config();
-var EchonetLite = require('node-echonet-lite');
+
+const express = require('express');
+const app = express();
+const EchonetLite = require('node-echonet-lite');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/b', async (req, res) => {
+    const value = await getMeasuredValue('FE80:0000:0000:0000:021C:6400:034D:BC97', [ 2, 136, 1 ])
+    res.json({
+      energy: value
+    })
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
 var el = new EchonetLite({
-  'lang'   : 'ja',
   'type'   : 'wisunb',
   'adapter': 'rl7023',
   'path'   : process.env.BROUTE_PATH,
   'id'     : process.env.BROUTE_ID,
   'pass'   : process.env.BROUTE_PASS,
-  'baud'   : 115200
 });
 
+// Initialize the EchonetLite object
 el.init((err) => {
-  if(err) {
-    console.log('[ERROR] '+ err.toString());
-    process.exit();
-  } else {
-    el.startDiscovery((err, res) => {
-      el.stopDiscovery();
-      if(err) {
-        console.log('[ERROR] '+ err.toString());
-        process.exit();
-      } else {
-        // Do something
-      }
-    });
-    el.on('data-serial', (res) => {
-      console.log(res['data']);
-    });
-    el.on('sent-serial', (res) => {
-      console.log('> ' + res['data']);
-    });
+  if(err) { // An error was occurred
+    showErrorExit(err);
+  } else { // Start to discover devices
+    discoverDevices();
   }
 });
+
+// Start to discover devices
+function discoverDevices() {
+  // Start to discover a smart electric energy meter
+  // on Wi-SUN B-route using a Wi-SUN USB dongle
+  el.startDiscovery((err, res) => {
+    // Error handling
+    if(err) {
+      showErrorExit(err);
+    }
+    // Determine the type of the found device
+    var device = res['device'];
+    var address = device['address'];
+    var eoj = device['eoj'][0];
+    var group_code = eoj[0]; // Class group code
+    var class_code = eoj[1]; // Class code
+    if(group_code === 0x02 && class_code === 0x88) {
+      // Stop to discovery process
+      el.stopDiscovery();
+      // This means that the found device belongs to
+      // the low voltage smart electric energy meter class
+      console.log('Found a smart electric energy meter (' + address + ').');
+    }
+  });
+}
+
+// Get the measured values
+const getMeasuredValue = (address, eoj) => {
+  var epc = 0xE7; // An property code which means "Measured instantaneous electric energy"
+  return new Promise(resolve => {
+    el.getPropertyValue(address, eoj, epc, (err, res) => {
+      resolve(res['message']['data']['energy']);
+    })
+  })
+}
+
+// Print an error then terminate the process of this script
+function showErrorExit(err) {
+  console.log('[ERROR] '+ err.toString());
+  process.exit();
+}
+
+function createLine(char) {
+  var len = process.stdout.columns - 1;
+  var line = '';
+  for(var i=0; i<len; i++) {
+    line += char;
+  }
+  return line;
+}
